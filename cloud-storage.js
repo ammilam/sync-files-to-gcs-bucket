4c38b5aafefa4214db20895abc8c1fe645540bf4
Andrew Milam <andrewmichaelmilam@gmail.com>
2023-01-17 14:31:39 -0600

    Create cloud-storage.js

---
+++ cloud-storage.js
@@ -0,0 +1,39 @@
+//
+// Cloud Storage Module
+// Uploads files to a gcs bucket
+//
+
+// Imports the Google Cloud storage client library
+const {
+  Storage
+} = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+// import modules
+const md5 = require('crypto-md5')
+let fs = require('fs')
+
+// create a file used to upload files to a gcs bucket
+async function uploadFile(bucketName, localPathToFile, localFileName) {
+
+  const options = {
+    destination: localFileName,
+  };
+
+  await storage.bucket(bucketName).upload(localPathToFile, options).catch(error => {
+    console.error(error)
+  }).then(response => {
+    let updated = response[0]['metadata']['updated']
+    let name = response[0]['metadata']['name']
+    let bucket = response[0]['metadata']['bucket']
+    if (updated) {
+      console.log(`${name} successfully uploaded to ${bucket}`)
+    }
+  })
+}
+
+// export the function for consumption
+module.exports = {
+  uploadFile
+}
