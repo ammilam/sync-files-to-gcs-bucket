83e181aa7f4756776eda080e6098bb1f8758336a
andrew milam <andrewmichaelmilam@gmail.com>
2023-02-05 22:30:01 -0600

    feat: secret manager secret sync

--- src/secret-manager/add-secret-version.js
+++ src/secret-manager/add-secret-version.js
@@ -1,48 +1,58 @@
 const fs = require("fs");
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+// Create a client instance of the Google Cloud Secret Manager service
+const client = new SecretManagerServiceClient();
 
+// Helper function to get the latest secret version
+async function getLatestSecret(project, secret) {
+    // Access the latest version of the specified secret
+    const [version] = await client.accessSecretVersion({
+        name: `projects/${project}/secrets/${secret}/versions/latest`,
+    });
+    // Return the secret data as a string
+    return version.payload.data.toString()
+}
+// Helper function to add a new secret version
+async function addSecretVersion(project, secret, payload) {
+    // Add a new version of the secret with the given payload
+    const [version] = await client.addSecretVersion({
+        parent: `projects/${project}/secrets/${secret}`,
+        payload: {
+            data: payload
+        }
+    });
+    // Return the newly added secret version
+    return version;
+}
+
+// Main function to add a new version of the secret
 async function addVersion(project, secret, path) {
-  const client = new SecretManagerServiceClient();
-  const fileSizeInKb = getFileSizeInKb(path);
-  const payload = getFilePayload(path);
+    // Get the file size in KB
+    const fileSizeInKb = await fs.statSync(path).size / 1024;
+    const p = await fs.readFileSync(path)
+    // Get the file payload
+    const payload = Buffer.from(p, 'utf8')
+    const checkPayload = payload.toString()
 
-  if (fileSizeInKb > 64) {
-    throw new Error(`File size of ${fileSizeInKb} KB exceeds the limit of 64 KB`);
-  }
 
-  const latestSecret = await getLatestSecret(client, project, secret);
-  console.log(latestSecret)
-  if (latestSecret !== payload) {
-    const version = await addSecretVersion(client, project, secret, payload);
-    console.log(`Added secret version ${version.name}`);
-  } else {
-    console.log("Latest secret version already matches the file contents");
-  }
-}
-
-function getFileSizeInKb(path) {
-  return fs.statSync(path).size / 1024;
-}
-
-function getFilePayload(path) {
-  return Buffer.from(fs.readFileSync(path), 'utf8');
-}
-
-async function getLatestSecret(client, project, secret) {
-  const [secretVersion] = await client.accessSecretVersion({
-    name: `projects/${project}/secrets/${secret}/versions/latest`,
-  });
-  return secretVersion.payload.data.toString();
-}
-
-async function addSecretVersion(client, project, secret, payload) {
-  const [version] = await client.addSecretVersion({
-    parent: `projects/${project}/secrets/${secret}`,
-    payload: {
-      data: payload
+    // Check if the file size exceeds the limit of 64 KB
+    if (fileSizeInKb > 64) {
+        throw new Error(`File size of ${fileSizeInKb} KB exceeds the limit of 64 KB`);
     }
-  });
-  return version;
+
+    // Get the latest version of the secret
+    const lS = await getLatestSecret(project, secret);
+    const latestSecret = Buffer.from(lS, 'utf8').toString()
+    // Check if the latest secret version matches the file contents
+    if (latestSecret !== checkPayload) {
+        // If not, add a new version of the secret with the file payload
+        const version = await addSecretVersion(project, secret, payload)
+        console.log(`Added secret version ${version.name}`);
+    } else {
+        // If the latest secret version already matches the file contents, log a message
+        console.log("Latest secret version already matches the file contents");
+    }
 }
 
+// Export the main function as a module
 module.exports.addSecretVersion = addVersion;
