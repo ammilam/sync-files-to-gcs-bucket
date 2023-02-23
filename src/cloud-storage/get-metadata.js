4b19e9e952af0ab54daad806f03072dcc1483bf0
Andrew Milam <andrewmichaelmilam@gmail.com>
2023-02-23 12:59:49 -0600

    feat: bucket path and better everything

--- src/cloud-storage/get-metadata.js
+++ src/cloud-storage/get-metadata.js
@@ -1,32 +1,57 @@
 const crypto = require('crypto');
 const fs = require('fs');
+
 const { Storage } = require('@google-cloud/storage');
 
+async function checkIfFileExists(bucketName, file, keyFile) {
+  try {
+    // initialize new storage client
+    const storage = new Storage({
+      keyFile
+    });
+    const [exists] = await storage.bucket(bucketName).file(file).exists()
+    return exists
+  } catch (error) {
+    console.error(error)
+  }
 
-async function getFileMetadata(bucketName, localPath, fileName, keyFile) {
+}
+
+async function getFileMetadata(bucketName, localPath, file, keyFile) {
+  // initialize new storage client
   const storage = new Storage({
     keyFile
   });
   try {
-    const [metadata] = await storage.bucket(bucketName).file(fileName).getMetadata();
-    const localContent = fs.readFileSync(localPath);
-    const localHash = crypto.createHash('md5').update(localContent).digest('base64');
-    const remoteHash = metadata.md5Hash;
-    return localHash === remoteHash ? 'matches' : 'no-match';
+    // invoke checkIfFileExists function to check if a file exists in the bucket
+    // that matches a file name matches
+    const exists = await checkIfFileExists(bucketName, file)
+    if (exists) {
+      const [metadata] = await storage.bucket(bucketName).file(file).getMetadata();
+      const localContent = fs.readFileSync(localPath);
+      const localHash = crypto.createHash('md5').update(localContent).digest('base64');
+      const remoteHash = metadata.md5Hash;
+      const response =  localHash == remoteHash ? 'matches' : 'no-match';
+      return response
+    } else {
+      return 'not-created'
+    }
   } catch (error) {
-    return 'not-created';
+    console.log(error);
   }
 }
 
 async function getBucketMetadata(bucketName, keyFile) {
+  // initialize new storage client
   const storage = new Storage({
     keyFile,
   });
   try {
+    // get bucket metadata, used to verify a bucket exists
     const [metadata] = await storage.bucket(bucketName).getMetadata();
     return metadata;
   } catch (error) {
-    console.log(error)
+    console.error(error)
     return false;
   }
 }
@@ -35,3 +60,4 @@ module.exports = {
   getFileMetadata,
   getBucketMetadata,
 };
+
